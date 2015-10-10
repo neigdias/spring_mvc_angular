67eb8b033329787601fe219e0b7fce6490e75aec
unknown <neigdias@gmail.com>
2015-10-10 14:50:57 -0300

    Updated the path project

---
+++ src/main/java/model/ProdutoEntity.java
@@ -0,0 +1,41 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="ProdutoEntity")
+public class ProdutoEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	int id;
+	String nome;
+	double preco;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+}
